e994275384a109511209c4ac2cb24fc273eb3829
Fabian Grimminger <grimmingerapps@gmail.com>
2018-03-23 19:47:21 +0100

    Refactoring and Test

--- src/main/steganography/ChangeEmbeder.java
+++ src/main/steganography/ChangeEmbeder.java
@@ -15,199 +15,176 @@ public class ChangeEmbeder implements DCTMatrixLSB {
 
 	Iterator<Integer> m_path;
 	Set<Integer> usedPositions = null;
-	
+
 	@Override
 	public List<List<DCTMatrix>> hideMessageWithKey(List<List<DCTMatrix>> stack, HiddenMessage message, String key) {
 		usedPositions = new HashSet<Integer>();
 		message = HiddenMessage.addHeaderInformation(message);
 		DCTStream stream = new DCTStream(stack);
 		this.createPath(stream, key);
-		
-		while(message.hasNext()) {
-			
-			int position;
-			try {
-				position = this.nextBitLocation();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				//e.printStackTrace();
-				return null;
-			}
-			
-			for(int offset = 1;offset<64 && message.hasNext();offset+=1) {
-				int value = stream.getValueAt(position+offset);
-				if(value==0 || value == 1) {
-					continue;
-				}
-				int bit = message.getNext();
-				this.usedPositions.add(position+offset);
-				if(bit!=Math.abs(value%2)) {
-					try {
-						this.doChange(stream, position+offset, /*diff*/500, bit);
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						//e.printStackTrace();
-						return null;
+		try {
+			while (message.hasNext()) {
+
+				int position = this.nextBitLocation();
+
+				for (int offset = 1; offset < 64 && message.hasNext(); offset++) {
+					int value = stream.getValueAt(position + offset);
+					if (value == 0 || value == 1) {
+						continue;
+					}
+					int bit = message.getNext();
+					this.usedPositions.add(position + offset);
+					if (bit != Math.abs(value % 2)) {
+						this.doChange(stream, position + offset, /* diff */500, bit);
 					}
 				}
+
 			}
-			
-		}		
+		} catch (NoPathException | NoChangePossibleException e) {
+			e.printStackTrace();
+			return null;
+		}
 		return stack;
 	}
-	
-	
 
 	@Override
 	public HiddenMessage recoverMessageWithKey(List<List<DCTMatrix>> stack, String key) {
 		DCTStream stream = new DCTStream(stack);
 		this.createPath(stream, key);
-		
-		int headerIndex = 0;
-		byte[] header = {0,0,0,0,0,-1};
+
+		byte[] header = { 0, 0, 0, 0, 0, -1 };
 		int messageLength = 0;
 		int bitsRead = 0;
-		int actual = 0;		
-		int position;
+		int actual = 0;
 		try {
-			position = this.nextBitLocation();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
+			e.printStackTrace();
 			return new HiddenMessage();
 		}
-		
-		int headerEndPos = 0;
-		int headerEndBit = 0;
-		
+
 		List<Byte> tempList = new ArrayList<Byte>();
-		
-		while(headerIndex<HiddenMessage.HEADERLENGTH) {
-			for(int offset = 1;offset<64;offset++) {
-				int value = stream.getValueAt(position+offset);
-				if(value==0 || value==1){//) || position+offset%64==0) {
+
+
+		int headerIndex = 0;
+		while (headerIndex < HiddenMessage.HEADERLENGTH) {
+			int position = 0;
+			try {
+				position = this.nextBitLocation();
+			} catch (NoPathException e1) {
+				e1.printStackTrace();
+			}
+			for (int offset = 1; offset < 64; offset++) {
+				int value = stream.getValueAt(position + offset);
+				if (value == 0 || value == 1) {
 					continue;
 				}
-				
-				actual*=(byte)2;
-				actual+=(byte)Math.abs(value%2);
+
+				actual *= (byte) 2;
+				actual += (byte) Math.abs(value % 2);
 				bitsRead++;
-				
-				if(bitsRead==8 && headerIndex<HiddenMessage.HEADERLENGTH) {
-					header[headerIndex] = (byte)actual;
+
+				if (bitsRead == 8 && headerIndex < HiddenMessage.HEADERLENGTH) {
+					header[headerIndex] = (byte) actual;
 					headerIndex++;
 					actual = 0;
 					bitsRead = 0;
-				}else if(bitsRead==8 && headerIndex>=5) {
-					//System.out.println("adding to list: "+actual);
-					tempList.add((byte)actual);
+				} else if (bitsRead == 8 && headerIndex >= 5) {
+					tempList.add((byte) actual);
 					actual = 0;
 					bitsRead = 0;
-					//break;
 				}
-				//if(headerIndex==5) {
-				//	headerEndPos = position;
-				//	headerEndBit = offset;
-				//	break;
-				//}
-			}
-			
-			try {
-				position = this.nextBitLocation();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				//e.printStackTrace();
-				return new HiddenMessage();
 			}
 		}
-		
-		if(header[5]!=0) {
+
+		if (header[5] != 0) {
 			System.err.println("Error in reconstruction of message");
 		}
-		
-		messageLength = HiddenMessage.getLengthInfo(header); // header[0]*256*256*256+header[1]*256*256+header[2]*256+header[3];
+
+		messageLength = HiddenMessage.getLengthInfo(header);
 		byte[] data = new byte[messageLength];
 		int index = 0;
-		
-		for(;index<tempList.size();index++) {
+
+		for (; index < tempList.size() && messageLength!=0; index++) {
 			data[index] = tempList.get(index);
 		}
 		
-		while(index<messageLength) {
-			for(int offset = 1;offset<64;offset++) {
-				
-				if(bitsRead==8) {
-					data[index] = (byte)actual;
+		while (index < messageLength) {
+			int position =0;
+			try {
+				position = this.nextBitLocation();
+			} catch (NoPathException e1) {
+				e1.printStackTrace();
+			}
+			for (int offset = 1; offset < 64; offset++) {
+
+				if (bitsRead == 8) {
+					data[index] = (byte) actual;
 					index++;
 					actual = 0;
 					bitsRead = 0;
 				}
-				if(index==messageLength) {
+
+				if (index == messageLength) {
 					break;
 				}
-				
-				int value = stream.getValueAt(position+offset);
-				if(value==0 || value==1){//) || position+offset%64==0) {
+
+				int value = stream.getValueAt(position + offset);
+				if (value == 0 || value == 1) {
 					continue;
-				}			
-				
-				actual*=(byte)2;
-				actual+=(byte)Math.abs(value%2);
+				}
+
+				actual *= (byte) 2;
+				actual += (byte) Math.abs(value % 2);
 				bitsRead++;
-				//if(headerIndex==5) {
-				//	headerEndPos = position;
-				//	headerEndBit = offset;
-				//	break;
-				//}
-			}		
-			try {
-				position = this.nextBitLocation();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				//e.printStackTrace();
-				return new HiddenMessage();
 			}
 		}
-		
+
 		Type type = Type.TEXT.ordinal() == header[4] ? Type.TEXT : Type.FILE;
-		return new HiddenMessage(data, type);		
+		return new HiddenMessage(data, type);
 	}
-	
-	
-	private void createPath(DCTStream stream, String key){
+
+	private void createPath(DCTStream stream, String key) {
 		Pathgenerator pg = new Pathgenerator(stream, key.hashCode());
 		this.m_path = pg.generatePath().iterator();
 	}
-	
-	private int nextBitLocation() throws Exception {
-		if(m_path.hasNext()) {
-			return m_path.next()*64;
+
+	private int nextBitLocation() throws NoPathException {
+		if (m_path.hasNext()) {
+			return m_path.next() * 64;
 		}
-		System.err.println("ERROR no more path Locations");
-		throw new Exception("ERROR no more path Locations");
+		throw new NoPathException("ERROR no more path Locations");
 	}
 
-	private void doChange(DCTStream stream, int startPos, int difference, int bit) throws Exception {
+	private void doChange(DCTStream stream, int startPos, int difference, int bit) throws NoChangePossibleException {
 		int startValue = stream.getValueAt(startPos);
-		int position = startPos+1;
+		int position = startPos + 1;
+		position %= (stream.size);
 		int nextValue = stream.getValueAt(position);
-		
-		while(position % 64 ==0 || Math.abs(nextValue-startValue)>difference || Math.abs(nextValue % 2) != bit || nextValue==1 || nextValue==0 || this.usedPositions.contains(position)) {
+
+		while (position % 64 == 0 || Math.abs(nextValue - startValue) > difference || Math.abs(nextValue % 2) != bit
+				|| nextValue == 1 || nextValue == 0 || this.usedPositions.contains(position)) {
 			position++;
-			position%=(stream.size);
+			position %= (stream.size);
 			nextValue = stream.getValueAt(position);
-			
-			if(position == startPos) {
-				//System.err.println("not possible!");
-				throw new Exception("not possible Change value found");
+
+			if (position == startPos) {
+				throw new NoChangePossibleException("not possible Change value found");
 			}
 		}
-		
+
 		stream.setValueAt(position, startValue);
 		stream.setValueAt(startPos, nextValue);
-		
-		//System.out.printf("Change %d with %d; Bit %d\n", startValue, nextValue, bit);
-		//System.out.printf("Start: %d\tEnd: %d\tIn: %b\n", startPos, position, this.usedPositions.contains(position));
-		
+	}
+
+	private class NoChangePossibleException extends Exception {
+		public NoChangePossibleException(String exceptionMessage) {
+			super(exceptionMessage);
+		}
+	}
+
+	private class NoPathException extends Exception {
+		public NoPathException(String exceptionMessage) {
+			super(exceptionMessage);
+		}
 	}
 }
--- src/test/steganography/TestChangeEmbeder.java
+++ src/test/steganography/TestChangeEmbeder.java
@@ -3,11 +3,15 @@ package test.steganography;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import main.JPEG.DCTMatrix;
 import main.JPEG.Matrix;
@@ -16,57 +20,91 @@ import main.steganography.ChangeEmbeder;
 import main.steganography.HiddenMessage;
 import main.steganography.HiddenMessage.Type;
 
+@RunWith(Parameterized.class)
 public class TestChangeEmbeder {
 	
 	ChangeEmbeder embeder;
-	final String crypto_key = "meinsuperKeyHIER";
-	final String stego_key = "super anderer key";
-	final String strmsg = "tolle Nachricht #super! 5+5*3-0/8, >|<,_-";
-	HiddenMessage msg;
+	final String crypto_key;// = "meinsuperKeyHIER";
+	final String stego_key;// = "super anderer key";
+	final String strmsg;// = "tolle Nachricht #super! 5+5*3-0/8, >|<,_-";
+	HiddenMessage hiddenmsg;
 	List<List<DCTMatrix>> imageData;
+	
+	@Parameters
+	public static List<Object[]> data(){
+		return Arrays.asList(new Object[][] {
+			{"meinsuperKeyHIER","super hprf34EOUf key",""},
+			{"meinsuperKeyHIER","super hprf34EOUf key","tolle Nachricht #super! 5+5*3-0/8, >|<,_-"},
+			{"1a1rkxuw56H6Q4xS","DGu68JPT6b anderer key","tolle Nachricht #super! 5+5*3-0/8, >|<,_-WFuM6oU80Zvgpe88Ysq8"},
+			{"xWsQtp7UiW9pDGHE","superbgHtuzgS7R anderer sydRtDGt7d","tolle 1aWrHniT1f2FfulQksTvNachricht #super! 5+5*3-0/8, >|<,_-"},
+			{"eoqOb3Z2O2EKKWHc","super andt5W9E60ngUerer key","tV3pxS3M442mizv8iDz4yolle Nachricht #super! 5+5*3-0/8, >|<,_-"},
+			{"tJMp9pCoeRHKMw7M","sSrNHkmFYGbuper anderer key","tolle Nachricht #super! 5iypscP4bckNZweydLxOr+5*3-0/8, >|<,_-"},
+			{"5lPx2Qx4x0vn66UJ","super anderer keydDP7CJgCV4","tolle Nachricht #super! 5+5*3-0/8, >JcXzt0qEmw1SqJQAOFdu|<,_-"},
+			{"JOPJeZBn1IRXXGPO","super anderer key","tolle Nachricht #sogdsmXCwD1ClaqJUP3e0uper! 5+5*3-0/8, >|<,_-"},
+			{"PAZSCFvWmg2PXHwa","super anderer prYG0qFwSWkey","tolle Nachnu0IdrDE04RqO7DS7Stlricht #super! 5+5*3-0/8, >|<,_-"},
+			{"uT8ielqtRtUqZfoR","super and5sVlCaFzD8erer key","tolle Nachricht #super! 5+5*3-0/8, >|<,_WjvHg9NrxpbCB2mN9jcp-"},
+			{"tVn9fXRuMvlUWW6Q","super annkwyZ4XYYLderer key","tolle NachrifQKJ3RDuLkwJBMcQL1vAcht #super! 5+5*3-0/8, >|<,_-"},
+			{"z7vQeLsa85auGAU1","super andereZWdsrxknWsr key","tolle Nachricht #super! 5+5GliCkk7kXsocZfvwqbgc*3-0/8, >|<,_-"},
+			{"aNniwTsklvo2fEPp","supeAEHSPXzSnBr anderer key","tolle NaEHqieh6SV38sHTlFPSe1chricht #super! 5+5*3-0/8, >|<,_-"},
+			{"57AHNglORRqNQPH3","super andAEHSPXzSnBerer key","tolle NachricBkCKHctKf3ce4KqDW26Xht #super! 5+5*3-0/8, >|<,_-"},
+			{"eGVpyULOFEpxN0SQ","su","tolle Nachricht1oaljluuQzgOKsUZqA9G #super! 5+5*3-0/8, >|<,_-"},
+			{"ykuimd3APvoHlhE8","super andVFZAq7HBmMerer key","tolle Nachricht #super! 5+5*3-0/8, >|<z9dxdGUzzqV6fbrqw0Pb,_-"},
+			{"oQqT2h2PcUjwtHkh","super anderer key","tolle NacheFzXcgywT39afh9T6TxNricht #super! 5+5*3-0/8, >|<,_-"},
+			{"SRdiQK9Z2awsu3uM","super anderer key","tolle Nachricht #super!Kc202y92UTdMP4COUk4k 5+5*3-0/8, >|<,_-"},
+			{"HGIY4cyfbBtfQ8ZZ","supegYqsx11iUgr anderer key","tolle Nachricht #super! 5+5*3-069HhTHdCw5sWJ1jwBtc0/8, >|<,_-"},
+			{"oOOGZWmisWQTI1zn","super hhOZL726HmrM6Hqanderer key","tolle NachqNhl0FnfADFFtF45Tadwricht #super! 5+5*3-0/8, >|<,_-"},
+			{"ZbCrsgJpAebqUTJe","super anhhOZL726HmrM6Hqderer key","tolle Nachricht #super! 5+5*3-0/8, VPOSLjwbX1IaKtXYUH4y>|<,_-"},
+			{"d9m7jU0sSHDATblM","super RLVrlvvKAjHUS6x key","tolla8TwzcEJGlN4GRC0VZBte Nachricht #super! 5+5*3-0/8, >|<,_-"}
+		});
+	}	
+	
+	public TestChangeEmbeder(String cryptoKey, String stegoKey, String msg) {
+		this.crypto_key = cryptoKey;
+		this.stego_key = stegoKey;
+		this.strmsg = msg;
+	}
 
 	@Before
 	public void setUp() throws Exception {
 		imageData = generateMatrixData();
 		embeder = new ChangeEmbeder();	
-		msg = new HiddenMessage(strmsg.getBytes(), Type.TEXT);
+		hiddenmsg = new HiddenMessage(strmsg.getBytes(), Type.TEXT);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		embeder = null;
 		imageData = null;
-		msg = null;
+		hiddenmsg = null;
 	}
 
 	@Test
-	public void testChangeEmbederWithKey() {
-		
-		msg = this.encrypt(msg);
+	public void testChangeEmbederWithKey() {		
+		HiddenMessage msg = this.encrypt(hiddenmsg);
 		List<List<DCTMatrix>> work = embeder.hideMessageWithKey(imageData, msg, this.stego_key);
 		HiddenMessage plainMsg = embeder.recoverMessageWithKey(work, this.stego_key);
-		plainMsg = this.encrypt(plainMsg);
-		assertEquals(msg, plainMsg);
+		plainMsg = this.decrypt(plainMsg);
+		assertEquals(hiddenmsg, plainMsg);
 	}	
 	
 	private HiddenMessage encrypt(HiddenMessage msg) {
 		SymmetricCryptography crypto = new SymmetricCryptography(this.crypto_key.getBytes());
-		byte[] data = crypto.encrypt(strmsg.getBytes());
+		byte[] data = crypto.encrypt(msg.getData());
 		return new HiddenMessage(data, msg.getType());
 	}
 	
 	private HiddenMessage decrypt(HiddenMessage msg) {
 		SymmetricCryptography crypto = new SymmetricCryptography(this.crypto_key.getBytes());
-		byte[] data = crypto.decrypt(strmsg.getBytes());
+		byte[] data = crypto.decrypt(msg.getData());
 		return new HiddenMessage(data, msg.getType());
 	}
 	
 	
 	private List<List<DCTMatrix>> generateMatrixData(){
 		List<List<DCTMatrix>> result = new ArrayList<List<DCTMatrix>>();
-		for(int i=0;i<3;i++) {
+		for(int i=0;i<4;i++) {
 			List<DCTMatrix> temp = new ArrayList<DCTMatrix>();
-			for(int j=0;j<3;j++) {
+			for(int j=0;j<4;j++) {
 								temp.add(generateMatrix());
 			}
 			result.add(temp);
